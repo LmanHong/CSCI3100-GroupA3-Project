8481712b224bf0ac6019a3bcc01d577d827a92be
estho3132 <62526689+estho3132@users.noreply.github.com>
2021-03-19 18:28:13 +0800

    Add (design of) classes and methods in model.py

--- project/friendSearch/models.py
+++ project/friendSearch/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.models import manager
 
-# Create your models here.
+class FrdSearchManager(models.manager):
+    #check whether the input is empty
+    def checkinput(self,college,study_field,hobbies,gender,behavior):
+        if not college and not study_field and not hobbies and not gender and not behavior:
+            raise ValueError("At least one of the row must be filled")
+        #...
+
+class FriendList(models.Model):
+    user1 = models.CharField(max_length=32)
+    user2 = models.CharField(max_length=32)
+
+    def createFrdList(self, user1, user2):
+        # store the link of them into database if user accept

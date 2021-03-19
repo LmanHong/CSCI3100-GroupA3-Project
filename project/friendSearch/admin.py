d764713a9f15c6c630c0840e3bcc8edc2cc5d841
estho3132 <62526689+estho3132@users.noreply.github.com>
2021-03-19 18:20:31 +0800

    Update admin.py

--- project/friendSearch/admin.py
+++ project/friendSearch/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from firendSearch.models import FrdSearch, FrdList
 
-# Register your models here.
+admin.site.register.(FrdSearch)
+admin.site.register.(FriendList)

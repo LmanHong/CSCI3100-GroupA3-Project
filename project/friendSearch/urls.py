c5d0d171814dd3892fb47c4cc86158396a075928
estho3132 <62526689+estho3132@users.noreply.github.com>
2021-04-04 13:35:50 +0800

    Update urls.py

--- project/friendSearch/urls.py
+++ project/friendSearch/urls.py
@@ -1,25 +1,10 @@
-"""project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
 import friendSearch.views
 
 urlpatterns = [
-    path('/frd_list', friendSearch.views.FriendList, name='frdList'),
-    path('/frd_search', friendSearch.views.FriendSearch, name='frdSearch')
+    path('frdList/', friendSearch.views.FriendList.as_view(), name='friendList'),
+    path('frdSearch/', friendSearch.views.FriendSearch.as_view(), name='friendSearch'),
 
 ]

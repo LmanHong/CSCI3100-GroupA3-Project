f6e3dcb725847c37f60fcb2bad68fadb1297f67d
estho3132 <62526689+estho3132@users.noreply.github.com>
2021-04-05 13:41:06 +0800

    Update views.py

--- project/friendSearch/views.py
+++ project/friendSearch/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth import login, authenticate, logout
+from friendSearch.forms import SearchForm
 from django.http import HttpResponse
+from .models import Requests, FriendList
 
-class FriendSearch(view):
-    def get(self,request):
-        return render(request,'template/frd_searching',{})
+class FriendSearchView(View):
+    def get(self, request, *args, **kwargs):
+        user = request.user
+        if user.is_authenticated:
+            context = {}
+            context['SearchForm'] = SearchForm()
+            return render(request,'frd_searching.html',context)
+        else:
+            print('You are not logging in')
+            return redirect('home')
     
-    def post(self,request):
-        pass
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        context = {}
+        fform = SearchForm(request.POST or None)
+        if fform.is_valid():
+            gender = request.POST['gender']
+            jobtitle = request.POST['jobtitle']
+            passion = request.POST['passions']
+            friendlist = FriendList.objects.SearchFrd(user,gender,jobtitle,passion)
+            context['frdlist'] = friendlist
+            #print(context['frdlist'])
+            return render(request,'frd_list.html',context)
+        else:
+            context['SearchForm'] = fform
+            return render(request,'frd_list.html',context)
         #to get the information of the form and send to the dbs
 
 class FriendList(view):

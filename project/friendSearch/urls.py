5141549674861a98997ee1a76c8388f8630cc0ce
estho3132 <62526689+estho3132@users.noreply.github.com>
2021-04-06 17:58:22 +0800

    Update urls.py

--- project/friendSearch/urls.py
+++ project/friendSearch/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 import friendSearch.views
 
 urlpatterns = [
-    path('frdList/', friendSearch.views.FriendList.as_view(), name='friendList'),
-    path('frdSearch/', friendSearch.views.FriendSearch.as_view(), name='friendSearch'),
+    path('frdList/', friendSearch.views.FriendListView.as_view(), name='friendList'),
+    path('frdSearch/', friendSearch.views.FriendSearchView.as_view(), name='friendSearch'),
+    path('request/',friendSearch.views.RequestView.as_view(), name='request'),
 
 ]

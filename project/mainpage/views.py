6a009872cdaa80b6dd652c6e71ce855e673b05f9
Bob-ho-z <62583059+Bob-ho-z@users.noreply.github.com>
2021-04-14 00:06:04 +0800

    remove

--- project/mainpage/views.py
+++ project/mainpage/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
-def main_page(self,request):
+def main_page(request):
     return render(request, 'mainpage/index.html')

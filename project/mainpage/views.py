aa48bb546fb893c3dfd77be420be0ddc94784166
Bob-ho-z <62583059+Bob-ho-z@users.noreply.github.com>
2021-04-14 16:20:01 +0800

    Update views.py

--- project/mainpage/views.py
+++ project/mainpage/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
-def main_page(request):
-    return render(request, 'mainpage/index.html')
+class main_page(View):
+    def get(self,request):
+        return render(request,'index.html')
+
+#def main_page(request):
+ #   return render(request, 'mainpage/index.html')
